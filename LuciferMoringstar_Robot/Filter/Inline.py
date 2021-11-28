b60852b7859bfadf3c2ea8e9db283688bc0b3b7a
Ivar-the-boneles <92593566+Ivar-the-boneles@users.noreply.github.com>
2021-11-28 12:54:29 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,7 +89,7 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
+        InlineKeyboardButton('SUB MY CHANNEL', url=f'https://www.youtube.com/channel/UCvSF3ur0GFRcsTxaOnLlWJA')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
